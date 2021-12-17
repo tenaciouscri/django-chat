0fdeabe9c62c7d3dffcc7d7535097938d598b05b
tenaciouscri <cridelve@gmail.com>
2021-12-17 13:10:29 +0100

    Added Selenium test

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -66,9 +66,12 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
     }
 }
 
